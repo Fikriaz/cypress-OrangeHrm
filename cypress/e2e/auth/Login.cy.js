659c74024a93e617329ee540d7127667d04558d2
Fikriaz <fikri.achmadfauzi77@gmail.com>
2025-07-17 23:50:52 +0700

    add test invalid scenario for auth

--- cypress/e2e/auth/Login.cy.js
+++ cypress/e2e/auth/Login.cy.js
@@ -1,4 +1,5 @@
 describe("Validation Login User",() => {
+
     it('User can login with valid username and password', ()=> {
     cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     
@@ -17,6 +18,7 @@ describe("Validation Login User",() => {
 })
 
 describe("Invalidation Login User",() => {
+
     it('User cannot login with valid username and invalid password', ()=> {
     cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     
@@ -29,7 +31,67 @@ describe("Invalidation Login User",() => {
     .click();
 
     //Assertion
-    cy.contains('.oxd-text','Dashboard')
+    cy.contains('.oxd-text','Invalid credentials');
+
     })
 
+
+    it('User cannot login with valid invalid username and valid password', ()=> {
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    
+    //fill form
+    cy.get('.oxd-input[name="username"]')
+    .type("user");
+    cy.get('.oxd-input[name="password"]')
+    .type('admin123');
+    cy.get('.oxd-button[type="submit"]')
+    .click();
+
+    //Assertion
+    cy.contains('.oxd-text','Invalid credentials');
+    })
+
+    it('User cannot login with valid username and password field is empty', ()=> {
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    
+    //fill form
+    cy.get('.oxd-input[name="username"]')
+    .type("Admin");
+    cy.get('.oxd-button[type="submit"]')
+    .click();
+
+    //Assertion
+    cy.contains('.oxd-text','需要')
+    })
+
+    it('User cannot login with username field is empty and valid password', ()=> {
+    
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    
+    //fill form
+    cy.get('.oxd-input[name="username"]')
+    .type("Admin");
+    cy.get('.oxd-input[name="password"]')
+    .type('admin123');
+    cy.get('.oxd-button[type="submit"]')
+    .click();
+
+    //Assertion
+   cy.contains('.oxd-text','需要')
+    })
+
+    it('User cannot login with username and password filed is empty', ()=> {
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    
+    //fill form
+    cy.get('.oxd-input[name="username"]')
+    .type("Admin");
+    cy.get('.oxd-input[name="password"]')
+    .type('admin123');
+    cy.get('.oxd-button[type="submit"]')
+    .click();
+
+    //Assertion
+    cy.contains('.oxd-text','需要')
+    })
 })
