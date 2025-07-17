3609541840eb86a9d4860630db48dc49c624e862
Fikriaz <fikri.achmadfauzi77@gmail.com>
2025-07-17 23:36:31 +0700

    add test valid scenario for auth

---
+++ cypress/e2e/auth/Login.cy.js
@@ -0,0 +1,35 @@
+describe("Validation Login User",() => {
+    it('User can login with valid username and password', ()=> {
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    
+    //fill form
+    cy.get('.oxd-input[name="username"]')
+    .type("Admin");
+    cy.get('.oxd-input[name="password"]')
+    .type('admin123');
+    cy.get('.oxd-button[type="submit"]')
+    .click();
+
+    //Assertion
+    cy.contains('.oxd-text','Dashboard')
+    })
+
+})
+
+describe("Invalidation Login User",() => {
+    it('User cannot login with valid username and invalid password', ()=> {
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    
+    //fill form
+    cy.get('.oxd-input[name="username"]')
+    .type("Admin");
+    cy.get('.oxd-input[name="password"]')
+    .type('user');
+    cy.get('.oxd-button[type="submit"]')
+    .click();
+
+    //Assertion
+    cy.contains('.oxd-text','Dashboard')
+    })
+
+})
